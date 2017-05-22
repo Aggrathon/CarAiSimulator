fb3087fbd31c83907ce8e284f6d849d9c82306a1
Aggrathon <antonbjo@gmail.com>
2017-05-22 03:43:28 +0300

    Better terrain generation

--- CarSimulator/Assets/TerrainGenerator.cs
+++ CarSimulator/Assets/TerrainGenerator.cs
@@ -9,15 +9,13 @@ public class TerrainGenerator : MonoBehaviour
 {
 
 	public bool generateOnStart = false;
-	[Space]
-	[Range(0f, 0.1f)]
-	public float largeScale = 0.01f;
-	[Range(0f, 0.2f)]
-	public float detailScale = 0.05f;
+	public DetailLayer[] detailLayers;
+	public Transform water;
 
 	Terrain terrain;
 	Thread thread;
 	float[,] tempHeights;
+	float waterHeight;
 
 	private void Awake()
 	{
@@ -36,25 +34,41 @@ public class TerrainGenerator : MonoBehaviour
 		tempHeights = null;
 		int size = terrain.terrainData.heightmapResolution;
 		float[,] heights = terrain.terrainData.GetHeights(0, 0, size, size);
-		float x1 = Random.Range(0f, 1000f);
-		float y1 = Random.Range(0f, 1000f);
-		float x2 = Random.Range(-1000f, 0f);
-		float y2 = Random.Range(-1000f, 0f);
-
+		float mapHeight = terrain.terrainData.heightmapScale.y;
+		float mapWidth = terrain.terrainData.heightmapWidth;
+		for (int h = 0; h < detailLayers.Length; h++)
+		{
+			detailLayers[h].x = Random.Range(-1000f, 1000f);
+			detailLayers[h].y = Random.Range(-1000f, 1000f);
+		}
 		thread = new Thread(() => {
+			float sumHeight = 0f;
 			for (int i = 0; i < size; i++)
 			{
 				for (int j = 0; j < size; j++)
 				{
-					float h = Mathf.PerlinNoise(x1 + i * largeScale, y1 + j * largeScale);
 					float px = (float)i / size * 2f - 1f;
 					float py = (float)j / size * 2f - 1f;
-					h += Mathf.Sqrt(px * px + py * py);
-					h += Mathf.PerlinNoise(x2 + i * detailScale, y2 + j * detailScale) * 0.2f;
-					h /= 2.2f; 
-					heights[i, j] = h;
+					float height = Mathf.Sqrt(px * px + py * py)-0.3f;
+					px = px * (float)mapWidth / 1000;
+					py = py * (float)mapWidth / 1000;
+					height = height*height * 0.6f + 0.4f;
+					for (int h = 0; h < detailLayers.Length; h++)
+					{
+						float detail = Mathf.PerlinNoise(
+							detailLayers[h].x + px * detailLayers[h].scale,
+							detailLayers[h].y + py * detailLayers[h].scale
+							) * 2 - 1;
+						if(detailLayers[h].detail)
+							height += Mathf.Clamp(detail * detail * detail, -0.75f, 0.75f) * detailLayers[h].height;
+						else
+							height += detail * detailLayers[h].height;
+					}
+					heights[i, j] = height;
+					sumHeight += height;
 				}
 			}
+			waterHeight = sumHeight / (1.8f * size * size) * mapHeight;
 			tempHeights = heights;
 		});
 		thread.Start();
@@ -67,6 +81,7 @@ public class TerrainGenerator : MonoBehaviour
 			yield return null;
 		terrain.terrainData.SetHeights(0, 0, tempHeights);
 		terrain.Flush();
+		water.position = new Vector3(water.position.x, waterHeight, water.position.z);
 		tempHeights = null;
 		thread = null;
 	}
@@ -75,5 +90,19 @@ public class TerrainGenerator : MonoBehaviour
 	{
 		if (thread != null)
 			thread.Abort();
+	}
+
+	[System.Serializable]
+	public struct DetailLayer
+	{
+		[Range(0f, 40f)]
+		public float scale;
+		[Range(0f, 0.5f)]
+		public float height;
+		public bool detail;
+		[System.NonSerialized]
+		public float x;
+		[System.NonSerialized]
+		public float y;
 	}
 }
