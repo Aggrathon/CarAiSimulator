b0418ab8c64eddde92adafe7fb96df3ec63a0713
Aggrathon <antonbjo@gmail.com>
2017-05-25 17:09:23 +0300

    Speedometer

---
+++ CarSimulator/Assets/Speedometer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speedometer : MonoBehaviour {
+
+	public Text speedText;
+	public Rigidbody car;
+
+	private void Update()
+	{
+		float speed = Vector3.Dot(car.velocity, car.transform.forward);
+		speedText.text = string.Format("{0 :0.0} km/h", speed*3.6f);
+	}
+}
