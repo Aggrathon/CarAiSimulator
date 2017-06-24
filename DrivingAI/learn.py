fe0715f923ad5cf58d081ad086755b416988c00d
Aggrathon <antonbjo@gmail.com>
2017-06-24 12:37:36 +0300

    Separeate the training variables

--- DrivingAI/learn.py
+++ DrivingAI/learn.py
@@ -5,4 +5,4 @@ from data import get_lane_shuffle_batch
 if __name__ == "__main__":
     tf.logging.set_verbosity(tf.logging.INFO)
     with DoubleNetwork(*get_lane_shuffle_batch(16)) as dn:
-        dn.learn(200000)
+        dn.learn(50000)
--- DrivingAI/model.py
+++ DrivingAI/model.py
@@ -13,10 +13,8 @@ class DoubleNetwork():
         os.makedirs(self.log_directory, exist_ok=True)
         os.makedirs(self.network_directory, exist_ok=True)
         self.global_step = tf.Variable(0, name='global_step')
-        with tf.variable_scope("Network_A"):
-            self.network_a = Network(images, variables, outputs, training, global_step=self.global_step)
-        with tf.variable_scope("Network_B"):
-            self.network_b = Network(images, variables, outputs, training)
+        self.network_a = Network(images, variables, outputs, training, global_step=self.global_step, name="Network_A")
+        self.network_b = Network(images, variables, outputs, training, name="Network_B")
         if training:
             self.saver = tf.train.Saver()
         else:
@@ -75,32 +73,35 @@ class DoubleNetwork():
 
 class Network():
 
-    def __init__(self, input_image, input_variables, example_output, training=True, global_step=None):
-        # Convolutional layers here
-        prev_layer = tf.layers.batch_normalization(input_image, training=training)
-        for i in [24, 32, 48]:
-            prev_layer = tf.layers.conv2d(prev_layer, i, [5,5], [2,2], 'valid', activation=tf.nn.relu)
-        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
-        prev_layer = tf.layers.conv2d(prev_layer, 64, [3,3], [1,1], 'valid', activation=tf.nn.relu)
-        # Combine variables
-        prev_layer = tf.contrib.layers.flatten(prev_layer)
-        prev_layer = tf.concat([prev_layer, input_variables], 1)
-        # Fully connected layers here
-        for i in [2048, 512, 128, 16]:
-            prev_layer = tf.layers.dense(
-                prev_layer, i, tf.nn.relu, 
+    def __init__(self, input_image, input_variables, example_output, training=True, global_step=None, name="Network"):
+        self.name = name
+        with tf.variable_scope(name):
+            # Convolutional layers here
+            prev_layer = tf.layers.batch_normalization(input_image, training=training)
+            for i in [24, 32, 48]:
+                prev_layer = tf.layers.conv2d(prev_layer, i, [5,5], [2,2], 'valid', activation=tf.nn.relu)
+            prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
+            prev_layer = tf.layers.conv2d(prev_layer, 64, [3,3], [1,1], 'valid', activation=tf.nn.relu)
+            # Combine variables
+            prev_layer = tf.contrib.layers.flatten(prev_layer)
+            prev_layer = tf.concat([prev_layer, input_variables], 1)
+            # Fully connected layers here
+            for i in [2048, 512, 128, 16]:
+                prev_layer = tf.layers.dense(
+                    prev_layer, i, tf.nn.relu, 
+                    use_bias=True, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.00001))
+                prev_layer = tf.layers.dropout(prev_layer, 0.3, training=training)
+            self.output = tf.layers.dense(prev_layer, 2, activation=tf.nn.tanh, 
                 use_bias=True, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.00001))
-            prev_layer = tf.layers.dropout(prev_layer, 0.3, training=training)
-        self.output = tf.layers.dense(prev_layer, 2, activation=tf.nn.tanh, 
-            use_bias=True, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.00001))
-        # Trainers and losses here
-        if example_output is not None:
-            self.loss = tf.losses.mean_squared_error(self.output, example_output) + tf.losses.get_regularization_loss()
-            adam = tf.train.AdamOptimizer(1e-5, 0.85)
-            self.trainer = adam.minimize(self.loss, global_step)
-            tf.summary.scalar('Loss', self.loss)
-        # Summaries
-        h, v = tf.split(self.output, [1,1], 1)
-        tf.summary.histogram('Horizontal', h)
-        tf.summary.histogram('Vertical', v)
+            # Trainers and losses here
+            self.vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=name)
+            if example_output is not None:
+                self.loss = tf.losses.mean_squared_error(self.output, example_output) + tf.losses.get_regularization_loss(name)
+                adam = tf.train.AdamOptimizer(1e-5, 0.85)
+                self.trainer = adam.minimize(self.loss, global_step, self.vars)
+                tf.summary.scalar('Loss', self.loss)
+            # Summaries
+            h, v = tf.split(self.output, [1,1], 1)
+            tf.summary.histogram('Horizontal', h)
+            tf.summary.histogram('Vertical', v)
 
